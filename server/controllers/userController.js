3f1b9d4f3cfdad58a915500cfed108a7ae717b1e
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-01-26 10:05:47 +0530

    controller and routes to get user profile and posts

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@
 
 import imagekit from "../configs/imagekit.js"
 import Connection from "../models/Connection.js"
+import Post from "../models/Post.js"
 import User from "../models/User.js"
 import fs from "fs"
 
@@ -260,6 +261,23 @@ export const acceptConnectionRequest=async(req,res)=>{
        
     } catch (error) {
          console.log(error)
+         res.json({success:false,message:error.message})
+    }
+}
+
+//get user profile
+export const getUserProfiles=async (req,res) => {
+    try {
+        const {profileId}=req.body
+        const profile=await User.findById(profileId)
+
+        if(!profile){
+            return res.json({success:false,message:"Profile not found"})
+        }
+        const posts=await Post.find({user:profileId}).populate('user')
+        res.json({success:true,profile,posts})
+    } catch (error) {
+        console.log(error)
          res.json({success:false,message:error.message})
     }
 }
